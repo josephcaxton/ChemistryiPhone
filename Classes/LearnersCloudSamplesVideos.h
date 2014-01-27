af0081c96ac1bb5a44f5a213abda220e19366c50
josephcaxton <josephcaxtonidowu@yahoo.com>
2014-01-27 14:32:14 +0000

    First Commit

---
+++ Classes/LearnersCloudSamplesVideos.h
@@ -0,0 +1,29 @@
+//
+//  LearnersCloudSamplesVideos.h
+//  EvaluatorForIPad
+//
+//  Created by Joseph caxton-Idowu on 21/02/2011.
+//  Copyright 2011 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "EvaluatorAppDelegate.h"
+
+@interface LearnersCloudSamplesVideos : UITableViewController {
+    
+	NSMutableArray *listofItems;
+	NSMutableArray *ImageNames;
+    UIButton *LCButton;
+	
+}
+
+@property (nonatomic, strong) NSMutableArray *listofItems;
+@property (nonatomic, strong) NSMutableArray *ImageNames;
+@property (nonatomic, strong) UIButton *LCButton;
+
+- (void)WebsitebuttonPressed;
+
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,18 @@
+
+//
+//  main.m
+//  Evaluator
+//
+//  Created by Joseph caxton-Idowu on 15/09/2010.
+//  Copyright caxtonidowu 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"EvaluatorAppDelegate");
+        return retVal;
+    }
+}
